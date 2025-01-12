46a70c36a6e135f28e11164f6d86c61a767f2730
Aqib1239 <114600200+Aqib1239@users.noreply.github.com>
2025-01-12 19:40:37 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,3 +1,3 @@
 import axios from 'axios';
 
-export const axiosInstance = axios.create({ baseURL: "http://localhost:5000/api", withCredentials: true });
+export const axiosInstance = axios.create({ baseURL: "https://chat-application-33tjbbolr-mohammad-aqibs-projects-8f45a5e2.vercel.app/api", withCredentials: true });
