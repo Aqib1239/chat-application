77b057c790992c930e0a117d944347515ce5d40e
Aqib1239 <114600200+Aqib1239@users.noreply.github.com>
2025-01-13 20:13:38 +0530

    Update HomePage.jsx

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,24 +1,38 @@
+
+
 import { useChatStore } from "../store/useChatStore";
 import ChatContainer from "../components/ChatContainer";
 import Sidebar from "../components/Sidebar";
 import NoChatSelected from "../components/NoChatSelected";
 
 const HomePage = () => {
-  const {selectedUser} = useChatStore();
+  const { selectedUser } = useChatStore();
 
   return (
     <div className="h-screen bg-base-200">
       <div className="flex items-center justify-center pt-[5.5rem] px-4">
         <div className="bg-base-100 rounded-lg shadow-cl w-full max-w-6xl h-[calc(100vh-8rem)]">
           <div className="h-full flex rounded-lg overflow-hidden">
-            <Sidebar />
+            {/* Sidebar */}
+            <div
+              className={`transition-all duration-300 ${
+                selectedUser ? "hidden sm:block " : "block"
+              } w-full sm:w-20`}
+            >
+              <Sidebar />
+            </div>
 
-            {!selectedUser ? <NoChatSelected /> : <ChatContainer />}
+            {/* Main Content */}
+            {!selectedUser ? (
+              <NoChatSelected className="hidden" />
+            ) : (
+              <ChatContainer />
+            )}
           </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default HomePage;
