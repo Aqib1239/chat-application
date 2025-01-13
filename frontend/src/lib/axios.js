7e6ea149d67792d8e2dabe933498c40411d189f5
Aqib1239 <114600200+Aqib1239@users.noreply.github.com>
2025-01-13 11:35:25 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,3 +1,3 @@
 import axios from 'axios';
 
-export const axiosInstance = axios.create({ baseURL: "https://chat-application-api-nine.vercel.app/api", withCredentials: true });
+export const axiosInstance = axios.create({ baseURL: "https://chat-application-api-nine.vercel.app/api", withCredentials: true, headers: { "Content-Type": "application/json" } });
