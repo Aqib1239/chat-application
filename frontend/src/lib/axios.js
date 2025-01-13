f6ed87f87222ac27b29287a069da855478014f09
Aqib1239 <114600200+Aqib1239@users.noreply.github.com>
2025-01-13 12:46:52 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,3 +1,3 @@
 import axios from 'axios';
 
-export const axiosInstance = axios.create({ baseURL: import.meta.env.MODE === "development" ? "https://chat-application-api-steel.vercel.app/api" : "/api", withCredentials: true });
+export const axiosInstance = axios.create({ baseURL: "https://chat-application-api-steel.vercel.app/api", withCredentials: true });
