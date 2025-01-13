f1777b35e45fb10c49608fb1f503289ae54d31aa
Aqib1239 <114600200+Aqib1239@users.noreply.github.com>
2025-01-13 12:54:33 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -30,13 +30,13 @@ app.use("/api/messages",messageRoute);
 
 
 // For deploying purpose
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static(path.join(__dirname, "../frontend/dist")));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-//   app.get("*", (req, res) => {
-//     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-//   });
-// }
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 // server is the socket server
 server.listen(PORT, ()=>
