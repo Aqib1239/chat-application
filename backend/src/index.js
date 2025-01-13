5e441bda1b3c33ebc1c6919f2a36f990500fc06e
Aqib1239 <114600200+Aqib1239@users.noreply.github.com>
2025-01-13 13:09:08 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -25,6 +25,15 @@ app.use(cors({
   credentials:true,
 }));
 
+// Global Header Middleware
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://quicktalk-application.vercel.app");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.header("Access-Control-Allow-Credentials", "true");
+  next();
+});
+
 app.use("/api/auth",authRoutes);
 app.use("/api/messages",messageRoute);
 
