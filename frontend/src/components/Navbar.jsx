eebd820585b7846629193b8a9fc6d42c2aa06de5
Aqib1239 <114600200+Aqib1239@users.noreply.github.com>
2025-01-13 20:14:41 +0530

    Update Navbar.jsx

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -56,7 +56,7 @@ const Navbar = () => {
           <div className="flex items-center gap-2">
             <Link
               to={"/settings"}
-              className={`btn btn-sm gap-2 transition-colors`}
+              className={`btn btn-sm rounded-full gap-2 transition-colors`}
             >
               <Settings className="w-5 h-5" />
               <span className="hidden sm:inline">Settings</span>
@@ -64,13 +64,13 @@ const Navbar = () => {
 
             {authUser && (
               <>
-                <Link to={"/profile"} className={`btn btn-sm gap-2`}>
-                  <User className="size-5" />
+                <Link to={"/profile"} className={`btn btn-sm rounded-full gap-2`}>
+                  <User className="w-5 h-5" />
                   <span className="hidden sm:inline">Profile</span>
                 </Link>
 
-                <button className="flex gap-2 items-center" onClick={logout}>
-                  <LogOut className="size-5" />
+                <button className="flex gap-2 rounded-full bg-red-500 px-3 py-1 items-center" onClick={logout}>
+                  <LogOut className="w-5 h-5" />
                   <span className="hidden sm:inline">Logout</span>
                 </button>
               </>
